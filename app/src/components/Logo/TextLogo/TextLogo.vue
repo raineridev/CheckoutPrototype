c360b273e635c7bdb20049f52887048f31dafc8f
Marcelo Raineri <marcelo@gutlz.com>
2024-10-21 23:51:46 +0100

    feat: adding components

---
+++ app/src/components/Logo/TextLogo/TextLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <h2 class="ml-14 text[32px] font-default font-extrabold ">Robustic</h2>
+    </div>
+</template>
+
+<style scoped lang="css">
+    div {
+        color: white;
+        height: 54px;
+        background-color: #18181B;
+    }
+</style>
